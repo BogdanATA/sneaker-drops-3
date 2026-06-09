f81532611334944fe89bb577ba2eb621c161a9a3
BogdanATA <batameev@gmail.com>
2026-06-09 13:47:41 -0400

    Created brand and sneaker repositories. seeded a few brands

--- src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
+++ src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
@@ -1,5 +1,7 @@
 package com.pluralsight.sneakerdrops;
 
+import com.pluralsight.sneakerdrops.data.BrandRepository;
+import com.pluralsight.sneakerdrops.models.Brand;
 import com.pluralsight.sneakerdrops.service.DropService;
 import com.pluralsight.sneakerdrops.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +12,30 @@ import org.springframework.stereotype.Component;
 public class StartupRunner implements CommandLineRunner {
     private final DropService dropService;
     private final InventoryService inventoryService;
+    private final BrandRepository brandRepository;
 
     @Autowired
-    public StartupRunner(DropService dropService, InventoryService inventoryService) {
+    public StartupRunner(DropService dropService, InventoryService inventoryService, BrandRepository brandRepository) {
         this.dropService = dropService;
         this.inventoryService = inventoryService;
+        this.brandRepository = brandRepository;
     }
 
     @Override
     public void run(String... args) {
-        System.out.println(dropService.getStatus());
-        System.out.println(inventoryService.getStatus());
+        seedData();
+
+        for (Brand brand : brandRepository.findAll()) {
+            System.out.println("Brand: " + brand.getId() + " - " + brand.getName());
+        }
+    }
+
+    private void seedData() {
+        if (brandRepository.count() == 0) {
+            brandRepository.save(new Brand("Nike"));
+            brandRepository.save(new Brand("Adidas"));
+            brandRepository.save(new Brand("New Balance"));
+            System.out.println("Brands seeded.");
+        }
     }
 }
